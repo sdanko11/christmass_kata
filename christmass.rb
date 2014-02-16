86aa27b34f6eb45aebebf369b6a7f13ad2a65b52
steve <stevedanko1@gmail.com>
2014-02-16 17:04:01 -0500

    finished working function

---
+++ christmass.rb
@@ -0,0 +1,10 @@
+def find_out_christmas_weekday(date)
+  if date.class == Time
+    return date.to_date.strftime("%A")
+  else
+    year = nil
+    date = date.split(" ")
+    date.each { |x| year = x if x.length == 4 }
+    Time.new(year, 12, 25).to_date.strftime("%A")
+  end
+end
