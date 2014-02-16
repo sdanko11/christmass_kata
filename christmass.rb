b78ff1d0bbc8955c67302fda88715bdcdceb8209
steve <stevedanko1@gmail.com>
2014-02-16 17:10:50 -0500

    removed date line wher I assigned the date varible

--- christmass.rb
+++ christmass.rb
@@ -3,8 +3,7 @@ def find_out_christmas_weekday(date)
     date.to_date.strftime("%A")
   else
     year = nil
-    date = date.split(" ")
-    date.each { |x| year = x if x.length == 4 }
+    date.split(" ").each { |x| year = x if x.length == 4 }
     Time.new(year, 12, 25).to_date.strftime("%A")
   end
 end
