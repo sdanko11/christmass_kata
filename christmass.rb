e7088dfe2acbb6675ebc9c59f726b9433c3413a5
steve <stevedanko1@gmail.com>
2014-02-17 11:15:21 -0500

    removed to date if the date is in the date class

--- christmass.rb
+++ christmass.rb
@@ -1,9 +1,9 @@
 def find_out_christmas_weekday(date)
   if date.class == Date
-    date.to_date.strftime("%A")
+    date.strftime("%A")
   else
     year = nil
     date.split(" ").each { |x| year = x if x.length == 4 }
-    Time.new(year, 12, 25).to_date.strftime("%A")
+    Time.new(year, 12, 25).strftime("%A")
   end
 end
